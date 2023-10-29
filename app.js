c5cc96bcc712cceef45d2e86ce8c7c70544624eb
Ingemar <ingemar.tamm@gmail.com>
2023-10-29 12:48:29 +0200

    ver2

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 const inputfield = document.getElementById('input-box');
 const list = document.getElementById('list');
-const circle1 = document.getElementById('ring1');
-const circle = document.getElementById('ring');
 
-words = [];
+
+let circle = document.getElementById('ring');
+
+
+let wheel = document.querySelector('.circle');
+let value = Math.ceil(Math.random() * 3600);
+let maxwords = 1;
+let wordindex = 1
 
 function addWord(){
     if (inputfield.value === '') {
         alert("Peab kirjutama midagi!");
     }
     else {
-        let addedWord = document.createElement('li');
-        addedWord.innerHTML = inputfield.value;
-        list.appendChild(addedWord);
-        words.push(inputfield.value);
-        let circleText = document.createElement('span')
-        let content = document.createTextNode(inputfield.value);
-        circleText.appendChild(content);
-        circle.appendChild(circleText);
-        console.log(circle1);
+        let words = inputfield.value.split('\n')
+        for (let word of words) {
+
+            console.log(word);
+            word = word.trim();
+            if (!word) continue;
+
+            let addedWord = document.createElement('li');
+            let circlesector = document.createElement('div');
+            let circlesectorcontent = document.createElement('span');
+
+
+            addedWord.innerHTML = word;
+            circlesectorcontent.innerHTML = word;
+
+
+            list.appendChild(addedWord);
+            circlesector.setAttribute('class', 'textoncircle')
+            circlesector.setAttribute('style', '--i:' + (wordindex) + ';')
+            
+            circlesector.appendChild(circlesectorcontent);
+            circle.appendChild(circlesector);
+            maxwords = Math.max(maxwords, wordindex)
+            wordindex += 1;
+        }
+        circle.setAttribute('style', '--words:' + maxwords + ';')
     }
     inputfield.value = '';
+}
+
+function spin() 
+{
+    wheel.style.transform = "rotate(" + value + "deg)";
+    value += Math.ceil(Math.random() * 3600);
 }
